743c17dccec9f99de5cf6859f742a23f7d9653c9
Jimmy Pham <jpham197@gmail.com>
2020-11-18 00:42:15 -0500

    Add accuracy calculation based on Angeline and Deepthi's hotmobile paper

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let viewPhrase = document.getElementsByClassName("phrase")[0];
 let speedView = document.getElementsByClassName("speed")[0];
 let accuracyView = document.getElementsByClassName("accuracy")[0];
 let startTime = null;
+let backspaceCount = 0;
 
 const loadNewPhrase = () => {
     let randomIndex = Math.random() * localPhrases.length;
@@ -11,22 +12,34 @@ const loadNewPhrase = () => {
     viewPhrase.innerHTML = phrase;
 }
 
-const calcAccuracy = () => {
-
+const calcAccuracy = (userInput) => {
+    let incorrectChar = 0;
+    for (let i = 0; i < userInput.innerHTML.length; i++) {
+        if (userInput.innerHTML[i] !== viewPhrase.innerHTML[i]) {
+            incorrectChar++;
+        }
+    }
+    console.log(incorrectChar + backspaceCount);
+    const errorRate = incorrectChar + backspaceCount;
+    const accuracy = Math.floor(((userInput.innerHTML.length - errorRate) / viewPhrase.innerHTML.length) * 100);
+    accuracyView.innerHTML = `Accuracy: ${accuracy}%`;
 }
 
-const calcSpeed = () => {
-    
-}
-
-const submitInput = (startTime, userInput) => {
-    console.log(startTime)
-    const endTime = new Date();
-    const totalTime = ((endTime.getTime() - startTime.getTime()) / 1000) / 60; //total time in minutes
+const calcSpeed = (totalTime, userInput) => {
     const wordsTyped = userInput.innerHTML.split(" ").length;
     const speed = wordsTyped / totalTime;
     speedView.innerHTML = `Speed: ${Math.floor(speed)} wpm`;
-    console.log(speed);
+}
+
+const submitInput = (startTime, userInput) => {
+    const endTime = new Date();
+
+    //total time in minutes
+    const totalTime = ((endTime.getTime() - startTime.getTime()) / 1000) / 60;
+
+    calcSpeed(totalTime, userInput);
+    calcAccuracy(userInput);
+
     return totalTime;
 }
 
@@ -50,6 +63,7 @@ window.addEventListener("keydown", (event) => {
             break;
         case "Backspace":
             userInput.innerHTML = userInput.innerHTML.slice(0, -1);
+            backspaceCount++;
             break;
         case "Shift":
             break;
